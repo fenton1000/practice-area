b300b0346b423077df6bd7627a7a5a638593de25
fenton1000 <fenton1000@yahoo.co.uk>
2023-01-04 12:13:06 +0000

    Further refine team selection to reduce bias towards team one

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 /**
- * Functions adds user inputted player name to player object array.
+ * Function addPlayer adds user inputted player name to player object array.
  */
 function addPlayer() {
     let newPlayer = document.getElementById('enter-name').value;
@@ -35,6 +35,10 @@ function addPlayer() {
     console.log(players);
 }
 
+/**
+ * function createNextTeams creates two team selections from the players array
+ * based on array index
+ */
 function createNextTeams() {
 
     let firstSort1 = [];
@@ -54,10 +58,56 @@ function createNextTeams() {
         }
     }
 
-    console.log(firstSort1);
-    console.log(firstSort2);
+    let teamA = [];
+    let teamAPart2 = [];
+    let teamB = [];
+    let teamBPart2 = [];
+
+    for (let i=0; i < firstSort1.length; i++) {
+
+        if (i === 0) {
+            let add = firstSort1[0];
+            teamA.push(add);
+        } else if (i % 2 === 0) {
+            let add = firstSort1[i];
+            teamA.push(add);
+        } else {
+            let add = firstSort1[i];
+            teamBPart2.push(add);
+        }
+    }
+
+    for (let i=0; i < firstSort2.length; i++) {
+
+        if (i === 0) {
+            let add = firstSort2[0];
+            teamB.push(add);
+        } else if (i % 2 === 0) {
+            let add = firstSort2[i];
+            teamB.push(add);
+        } else {
+            let add = firstSort2[i];
+            teamAPart2.push(add);
+        }
+    }
+
+    for (player of teamAPart2) {
+        teamA.push(player);
+    }
+
+    for (player of teamBPart2) {
+        teamB.push(player);
+    }
+
+    console.log(teamA);
+    console.log(teamB);
+
 }
 
+/**
+ * Function updatePlayerPoints takes match results inputted by the user
+ * and updates player points in the player array accordingly.
+ */
 function updatePlayerPoints() {
 
 }
